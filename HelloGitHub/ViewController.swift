e9c76288b4d8c4958e8bc14a782ad482352c3d69
David Dörflinger <david@davids-macbook-pro.local>
2018-12-23 12:59:11 +0100

    implemented some code

--- HelloGitHub/ViewController.swift
+++ HelloGitHub/ViewController.swift
@@ -6,15 +6,16 @@
 //  Copyright © 2018 David Dörflinger. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // test git
+        print("testing the GitHub stuff")
     }
 
-
 }
-
